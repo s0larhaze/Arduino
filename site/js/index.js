65f4c2420582dc0a223d38c030cbe78f58a1784a
ChelGame <chel.06081995@gmail.com>
2024-03-12 22:52:01 +0300

    Remove continue

--- site/js/index.js
+++ site/js/index.js
@@ -73,7 +73,6 @@ class App {
                         });
                         this.printObjects(this.data);
                     }
-                    continue;
                 }
             });
         });
