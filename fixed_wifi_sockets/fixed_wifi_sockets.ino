a1b985ace2e24d79a50c3b70ce7e7c9e5326550f
s0larhaze <drearyengineer@gmail.com>
2024-03-21 00:20:21 +0300

    Some serious work

--- fixed_wifi_sockets/fixed_wifi_sockets.ino
+++ fixed_wifi_sockets/fixed_wifi_sockets.ino
@@ -13,9 +13,9 @@ const unsigned long MEASURE_DELAY = 5 * SECONDS;
 const unsigned long MEASURE_FOR = 10 * SECONDS;
 const unsigned long MOCK_EMERGENCY_LIMIT = 20 * SECONDS;
 
-const unsigned int OBJECT_ID = 1;
+int OBJECT_ID = 1;
 
-enum class MessageType{
+enum class MessageType {
   MESSAGE_STARTED_MEASURING = 0,
   MESSAGE_FINISHED_MEASURING = 1,
   MESSAGE_EMERGENCY = 2
@@ -28,6 +28,7 @@ using namespace websockets;
 WebsocketsClient client;
 
 void onMessageCallback(WebsocketsMessage message) {
+  Serial.println("HI");
   Serial.println("Message: " + message.data());
 
   DynamicJsonDocument doc(1024);
@@ -46,46 +47,70 @@ void onMessageCallback(WebsocketsMessage message) {
   Serial.print("data: ");
   Serial.println(data);
 
-  switch ()
+  if (strcmp(type, "executePlannedMeasurement") == 0) {
+    Serial.println("Execute planned measurement event triggered");
+    measureBattery();
+  } 
+  
+  if (strcmp(type, "startMockEmergency") == 0) {
+    Serial.println("Emergency event triggered");
+    emergency();
+  }
 }
 
 void onEventsCallback(WebsocketsEvent event, String data) {
-    if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
-    } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
-    } else if(event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if(event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
+  if (event == WebsocketsEvent::ConnectionOpened) {
+    Serial.println("Connnection Opened");
+
+    JsonDocument regInfo;
+    regInfo["type"] = "arduinoObjectRegistration";
+    regInfo["data"]["object_id"] = OBJECT_ID;
+    String regInfoSerialized;
+    serializeJson(regInfo, regInfoSerialized);
+
+    client.send(regInfoSerialized);
+  } else if (event == WebsocketsEvent::ConnectionClosed) {
+    Serial.println("Connnection Closed");
+    while (!client.available()) {
+      Serial.println("Trying to form a socket connection...");
+      client.connect("ws://192.168.216.231:3000");
+      delay(1000);
     }
+  } else if (event == WebsocketsEvent::GotPing) {
+    Serial.println("Got a Ping!");
+  } else if (event == WebsocketsEvent::GotPong) {
+    Serial.println("Got a Pong!");
+  }
 }
 
 void emergency() {
   unsigned long emergency_time = 0;
 
   float voltage = analogRead(VOLTAGE_IN) * (5.0 / 1023.0);
-  float current = (voltage - 2.5) / 0.185; //analogRead(CURRENT_IN);
+  float current = (voltage - 2.5) / 0.185;  //analogRead(CURRENT_IN);
 
-  while (emergency_time < MOCK_EMERGENCY_LIMIT){
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(10);
-      digitalWrite(LED_BUILTIN, LOW);
-      JsonDocument jsdoc;
+  while (emergency_time < MOCK_EMERGENCY_LIMIT) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10);
+    digitalWrite(LED_BUILTIN, LOW);
+    JsonDocument jsdoc;
 
-      jsdoc["message_type"] = int(MessageType::MESSAGE_EMERGENCY);
-      jsdoc["object_id"] = OBJECT_ID;
-      jsdoc["voltage"] = voltage;
-      jsdoc["current"] = current;
 
-      serializeJson(jsdoc, Serial);
-      Serial.println("");
-      delay(MEASURE_DELAY);
-      emergency_time += MEASURE_DELAY;
+    jsdoc["type"] = "arduinoEmergency";
+    jsdoc["data"]["object_id"] = OBJECT_ID;
+    jsdoc["data"]["voltage"] = voltage;
+    jsdoc["data"]["current"] = current;
+    String jsdocSerialized;
+    serializeJson(jsdoc, jsdocSerialized);
+
+    client.send(jsdocSerialized);
+
+    delay(MEASURE_DELAY);
+    emergency_time += MEASURE_DELAY;
   }
 }
 
-void measureBattery(){
+void measureBattery() {
   float voltage_total = 0.0;
   float current_total = 0.0;
 
@@ -93,12 +118,23 @@ void measureBattery(){
   unsigned long measured_times = 0;
 
   digitalWrite(LED_BUILTIN, HIGH);
-  JsonDocument measurementStart;
-  Serial.printf("{ \"message_type\": 0, \"object_id\": %i }\n", OBJECT_ID);
+  JsonDocument startMeasurementData;
+  JsonDocument data;
 
-  while (true){
+  data["object_id"] = OBJECT_ID;
+  String serializedData;
+  serializeJson(data, serializedData);
+
+  startMeasurementData["type"] = "arduinoStartedMeasurement";
+  startMeasurementData["data"]["object_id"] = OBJECT_ID;
+  String serializedSMD;
+  serializeJson(startMeasurementData, serializedSMD);
+  
+  client.send(serializedSMD);
+
+  while (true) {
     float voltage = analogRead(VOLTAGE_IN) * (5.0 / 1023.0);
-    float current = (voltage - 2.5) / 0.185; //analogRead(CURRENT_IN);
+    float current = (voltage - 2.5) / 0.185;  //analogRead(CURRENT_IN);
 
     voltage_total += voltage;
     current_total += current;
@@ -112,10 +148,12 @@ void measureBattery(){
       jsdoc["object_id"] = OBJECT_ID;
       jsdoc["avg_voltage"] = voltage_total / float(measured_times);
       jsdoc["avg_current"] = current_total / float(measured_times);
+      String jsonStringified;
 
       digitalWrite(LED_BUILTIN, LOW);
-      serializeJson(jsdoc, Serial);
-      Serial.println("");
+      serializeJson(jsdoc, jsonStringified);
+      Serial.println(jsonStringified);
+      client.send("{ \"type\": \"arduinoFinishedMeasurement\", \"data\": " + jsonStringified + " }");
       break;
     }
 
@@ -125,19 +163,19 @@ void measureBattery(){
 }
 
 void setup() {
-    Serial.begin(115200);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.println("Connecting to WiFi...");
-        delay(1000);
-    }
-    Serial.println("Connected to WiFi");
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to WiFi...");
+    delay(1000);
+  }
+  Serial.println("Connected to WiFi");
 
-    client.onMessage(onMessageCallback);
-    client.onEvent(onEventsCallback);
-    client.connect("ws://192.168.216.231:3000");
+  client.onMessage(onMessageCallback);
+  client.onEvent(onEventsCallback);
+  client.connect("ws://192.168.216.231:3000");
 
-    // You can send messages to the server using client.send() method
+  // You can send messages to the server using client.send() method
 }
 
 void loop() {
--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const app = express();
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
+var connectedObjects = new Map(); // not working, gotta find a better solution
+
 app.use(express.json());
 app.use("/socket", express.static('socket'))
 app.use(cors({
@@ -21,8 +23,7 @@ app.use(cors({
 }))
 
 wss.on('connection', (ws) => {
-  ws.id = uuid.v4();
-  console.log("Connection! " + ws.id);
+  console.log("Connection!");
 
   ws.on('message', (message) => {
     handleMessage(message, ws);
@@ -33,58 +34,345 @@ wss.on('connection', (ws) => {
   });
 })
 
-// const port = new serial.SerialPort({ path: "/dev/ttyUSB0", baudRate: 9600 });
-// const parser = port.pipe(new ReadlineParser());
-// parser.on('data', arduinoMessageHandler)
-//
+function handleMessage(message, ws) {
+  const type = JSON.parse(message).type;
+  const data = JSON.parse(message).data;
 
-function plannedMeasurementHandler(object_id, ws) {
-  console.log(object_id);
+  console.log(JSON.parse(message));
+  console.log(type);
+  console.log(data);
+  object_id = data.object_id;
 
-  wss.clients.forEach(element => {
-    element.send(JSON.stringify({ type: 'getObjectMeasurementDataResponse', data: "Hello" }));
-  });
-  // ws.send(JSON.stringify({ type: 'getObjectMeasurementDataResponse', data: "Hello" }));
+  switch (type) {
+    case 'arduinoStartedMeasurement':
+      measurementStartedDBOperation(object_id);
+      break;
+    case 'arduinoFinishedMeasurement':
+      measurementFinishedDBOperation(data.avg_current, data.avg_voltage, object_id);
+      break;
+    case 'arduinoEmergency':
+      emergencyHandler(data.current, data.voltage, object_id);
+      break;
+    case 'arduinoEmergencyStopped':
+      emergencyStoppedHandler(object_id);
+      break;
+    case 'arduinoObjectRegistration':
+      objectRegistrationHandler(object_id, ws);
+      break;
+    case 'executePlannedMeasurement':
+      object_socket = connectedObjects.get(object_id); // not working, gotta find a better solution
+      executePlannedMeasurementHandler(object_id, object_socket);
+      break;
+    case 'getObjectList':
+      getObjectListHandler(ws);
+      break;
+    case 'getObjectMeasurementData':
+      getObjectMeasurementDataHandler(object_id, ws);
+      break;
+    case 'startMockEmergency':
+      object_socket = connectedObjects.get(object_id); // not working, gotta find a better solution
+      startMockEmergencyHandler(object_id, object_socket);
+      break;
+    case 'reset':
+      resetHandler(object_id);
+      break;
+    default:
+      console.log('Unknown message type:', type);
+  }
 }
 
+function emergencyStoppedHandler() {
+  // todo
+}
 
-function handleMessage(message, ws) {
-  // Handle different types of messages
-  console.log(JSON.parse(JSON.stringify(message)));
-  try {
-    const data = JSON.parse(message);
-    switch (data.type) {
-      case 'executePlannedMeasurementRequest':
-        plannedMeasurementHandler(data.data.object_id, ws);
-        break;
-      case 'getListOfObjectsRequest':
-        sqlcon.query(`SELECT obj.id, name, status, start_timestamp AS timestamp
-                              FROM objects AS obj LEFT JOIN measurements AS m ON obj.id = m.object_id 
-                              ORDER BY timestamp DESC`, (err, result) => {
-          if (err) throw err;
-          ws.send(JSON.stringify({ type: 'getListOfObjectsResponse', data: result }));
-        });
-        break;
-      case 'getObjectMeasurementDataRequest':
-        const { object_id } = data;
-        sqlcon.query(`SELECT * FROM measurements WHERE object_id = ${object_id}`, (err, result) => {
-          if (err) throw err;
-          ws.send(JSON.stringify({ type: 'getObjectMeasurementDataResponse', data: result }));
-        });
-        break;
-      case 'startMockEmergencyRequest':
-        port.write("emergency", (err) => {
-          if (err) throw err;
-        });
-        break;
-      case 'hello':
-        ws.send(JSON.stringify({ type: 'HelloResponse', data: "Hello you too!" }));
-      default:
-        console.log('Unknown message type:', data.type);
-    }
-  } catch (error) {
-    console.error('Failed to parse message:', message);
+function startMockEmergencyHandler(object_id, ws) {
+  wss.clients.forEach(elem => {
+    elem.send(JSON.stringify({ type: "startMockEmergency", data: object_id }));
+  })
+}
+
+function getObjectMeasurementDataHandler(object_id, ws) {
+  if (object_id === '') {
+    console.log("object_id cannot be empty");
+    return;
   }
+
+  sqlcon.query(`SELECT * FROM measurements WHERE object_id = ${object_id}`, (err, result) => {
+    if (err) throw err;
+    ws.send(JSON.stringify({ type: 'getObjectMeasurementData', data: result }));
+  });
+}
+
+function emergencyHandler(current, voltage, object_id) {
+  sqlcon.query(`insert into emergency (current, voltage, timestamp, object_id) values 
+  (
+    ${current},
+    ${voltage},
+    '${moment().format('YYYY-MM-DD HH:mm:ss')}',
+    ${object_id}
+  )`, err => {
+    if (err) throw err;
+  })
+}
+
+function objectRegistrationHandler(object_id, ws) {
+
+  if (object_id === '') {
+    console.log("object_id cannot be empty");
+    return;
+  }
+
+  checkIfObjectAlreadyRegistered = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`select count (*) as count from objects where id = ${object_id}`, (err, result) => {
+        if (err) reject(err);
+        resolve(result[0].count);
+      });
+    })
+  }
+
+  registerObject = () => {
+    sqlcon.query(`insert into objects (id, name, status) values 
+    (
+      ${object_id},
+      'Безымянный объект',
+      0
+    )`, (err) => {
+      if (err) throw err;
+    })
+  }
+
+  checkIfObjectAlreadyRegistered()
+    .then(count => {
+      if (count === 0) {
+        registerObject();
+      }
+    })
+    .then(() => connectedObjects.set(object_id, ws))
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+function resetHandler(object_id) {
+
+  if (object_id === '') {
+    console.log("object_id cannot be empty");
+    return;
+  }
+
+  createArchiveDBQuery = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query("create database if not exists archive_battery", (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  };
+
+  useArchiveBatteryQuery = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query("use archive_battery", (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  };
+
+  createArchiveTablesQuery = () => {
+    return new Promise((resolve, reject) => {
+
+      createObjectsTable = () => {
+        return new Promise((resolve, reject) => {
+          sqlcon.query(`create table if not exists objects 
+        (
+        id int primary key,
+          name text,
+          status tinyint
+        )`, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+          })
+        })
+      }
+
+      createArchiveMeasurementsTable = () => {
+        return new Promise((resolve, reject) => {
+          sqlcon.query(`create table if not exists measurements 
+          (
+            id int auto_increment primary key, 
+            avg_current float,
+            avg_voltage float,
+            start_timestamp timestamp,
+            end_timestamp timestamp,
+            object_id int,
+            isReferential boolean
+          )`, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+          })
+        })
+      }
+
+      createArchiveEmergencyTable = () => {
+        return new Promise((resolve, reject) => {
+          sqlcon.query(`create table if not exists emergency 
+          (
+            id int auto_increment primary key,
+            current float,voltage float,
+            timestamp timestamp,
+            object_id int
+          )`, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+          })
+        })
+      }
+
+      createArchiveObjectsTable()
+        .then(createMeasurementsTable)
+        .then(createEmergencyTable)
+        .then(() => {
+          resolve();
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        })
+    });
+  };
+
+  createArchiveDBQuery
+    .then(useArchiveBatteryQuery)
+    .then(createArchiveTablesQuery)
+    .then(() => {
+      console.log("Archive DB is ready...");
+    })
+    .then(useBatteryQuery)
+    .catch((err) => {
+      console.log(err);
+    });
+
+  dumpMeasurementRecords = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`insert into archive_battery.measurements select * from battery.measurements where object_id = ${object_id}`, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      })
+    })
+  }
+
+  dumpEmergencyRecords = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`insert into archive_battery.emergency select * from battery.emergency where object_id = ${object_id}`, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      })
+    })
+  }
+
+  resetMeasurementRecords = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`delete from measurements where object_id = ${object_id}`, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      })
+    })
+  }
+
+  resetEmergencyRecords = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`delete from emergency where object_id = ${object_id}`, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      })
+    })
+  }
+
+  dumpMeasurementRecords()
+    .then(dumpEmergencyRecords)
+    .then(resetMeasurementRecords)
+    .then(resetEmergencyRecords)
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function measurementStartedDBOperation(object_id) {
+
+  if (object_id === '') {
+    console.log("object_id cannot be empty");
+    return;
+  }
+
+  checkIfRecordsExist = () => {
+    return new Promise((resolve, reject) => {
+      sqlcon.query(`select count(*) as count from measurements`, (err, result) => {
+        if (err) reject(err);
+        resolve(result[0].count);
+      })
+    })
+  }
+
+  insertRecordWithReferentialFlag = () => {
+    console.log("Trying to insert with referential flag...");
+    sqlcon.query(`insert into measurements 
+      (start_timestamp, object_id, isReferential) values ('${moment().format('YYYY-MM-DD HH:mm:ss')}', ${object_id}, 1)`, (err) => {
+      if (err) throw err;
+      console.log("Succesfully inserted.");
+    })
+  }
+
+  insertRecord = () => {
+    sqlcon.query(`insert into measurements 
+      (start_timestamp, object_id) values ('${moment().format('YYYY-MM-DD HH:mm:ss')}', ${object_id})`, (err) => {
+      if (err) throw err;
+    })
+  }
+
+  checkIfRecordsExist()
+    .then(count => {
+      if (count === 0) {
+        insertRecordWithReferentialFlag();
+      } else {
+        insertRecord();
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+}
+
+function measurementFinishedDBOperation(avg_current, avg_voltage, object_id) {
+  sqlcon.query(`update measurements set 
+    avg_current = ${avg_current},
+    avg_voltage = ${avg_voltage},
+    end_timestamp = '${moment().format('YYYY-MM-DD HH:mm:ss')}',
+    isReferential = 0
+    where object_id = ${object_id} and end_timestamp is NULL`, (err) => {
+    if (err) throw err;
+  })
+}
+
+function executePlannedMeasurementHandler(object_id, object_socket) {
+  if (object_id === '') {
+    console.log("object_id cannot be empty");
+    return;
+  }
+
+  object_socket.send(JSON.stringify({ type: "executePlannedMeasurement" }));
+
+  // wss.clients.forEach((client) => {
+  //   client.send(JSON.stringify({ type: "executePlannedMeasurement" }));
+  // })
+}
+
+function getObjectListHandler(ws) {
+  sqlcon.query(`SELECT obj.id, name, status, start_timestamp AS timestamp
+                      FROM objects AS obj LEFT JOIN measurements AS m ON obj.id = m.object_id 
+                      ORDER BY timestamp DESC`, (err, result) => {
+    if (err) throw err;
+    ws.send(JSON.stringify({ type: 'getObjectList', data: result }));
+  });
 }
 
 function arduinoMessageHandler(message) {
@@ -199,7 +487,7 @@ createTablesQuery = () => {
       return new Promise((resolve, reject) => {
         sqlcon.query(`create table if not exists objects 
         (
-        id int auto_increment primary key,
+        id int primary key,
           name text,
           status tinyint
         )`, (err, result) => {
@@ -213,12 +501,13 @@ createTablesQuery = () => {
       return new Promise((resolve, reject) => {
         sqlcon.query(`create table if not exists measurements 
           (
-          id int auto_increment primary key, 
+            id int auto_increment primary key, 
             avg_current float,
             avg_voltage float,
             start_timestamp timestamp,
             end_timestamp timestamp,
-            object_id int
+            object_id int,
+            isReferential boolean
           )`, (err, result) => {
           if (err) reject(err);
           resolve(result);
@@ -272,96 +561,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// app.post('/api/getListOfObjects', (req, res) => {
-//   sqlcon.query("select * from objects", (err, objects) => {
-//     if (err) throw err;
-//     // res.type("json");
-//     // res.send(result);
-//
-//     objects.forEach(object => {
-//       if (object.status == objectStates.OBJECT_MEASURING) {
-//         sqlcon.query(`select start_timestamp from measurements 
-//           where object_id = ${object.id} and end_timestamp = NULL`, (err, result) => {
-//           object.timestamp = result.start_timestamp;
-//           console.log(object);
-//         })
-//       }
-//     });
-//   })
-// });
-//
-// app.post('/api/getObjectMeasurementData', (req, res) => {
-//
-//   console.log("Got getData query!");
-//
-//   selectAll = () => {
-//     return new Promise((resolve, reject) => {
-//       sqlcon.query("select * from measurements", (err, result) => {
-//         if (err) reject(err);
-//         resolve(result);
-//       });
-//     });
-//   }
-//
-//   selectAll()
-//     .then((result) => {
-//       console.log(result);
-//       res.type("json");
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-//
-// app.post('/api/executePlannedMeasurement', (req, res) => {
-//   object_id = parseInt(req.body.object_id);
-//   console.log(object_id);
-//
-//   console.log(parser.listenerCount());
-//
-//   // parser.on('data', (data) => {
-//   //   console.log("Got arduino's answer: " + data);
-//   //
-//   //   let buffer = "";
-//   //   buffer += data;
-//   //   const start = buffer.indexOf('{');
-//   //   const end = buffer.lastIndexOf('}');
-//   //   if (start !== -1 && end !== -1 && start < end) {
-//   //     const jsonString = buffer.substring(start, end + 1);
-//   //     try {
-//   //       const jsonData = JSON.parse(jsonString);
-//   //       console.log(jsonData);
-//   //
-//   //       res.type('json');
-//   //       res.send(jsonData);
-//   //     } catch (error) {
-//   //       console.error('Error parsing JSON:', error);
-//   //       console.log('Raw JSON string:', jsonString);
-//   //     }
-//   //     buffer = buffer.substring(end + 1);
-//   //   }
-//   // });
-//
-//   port.write("measure", (err) => {
-//     if (err) throw err;
-//   });
-// })
-//
-// app.post('/api/startMockEmergency', (req, res) => {
-//   console.log(req.body);
-//
-//   port.write("emergency", (err) => {
-//     if (err) throw err;
-//   });
-// })
-
 server.listen(3000, () => {
-  console.log("Ran server on http://localhost:3000");
+  console.log("Server is running...");
 });
-
-// Get objects: get all the data of objects from the db
-// Get object data: get all the measurement data of the object from the db
-// Send measure request: 
-// Get emergency data: 
-
