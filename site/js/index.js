d1c85e5e11f1a9c57de2554f734da88b4d9c6016
ChelGame <chel.06081995@gmail.com>
2024-03-30 22:49:27 +0300

    Poshol Na 10

--- site/js/index.js
+++ site/js/index.js
@@ -192,7 +192,7 @@ class App {
                 case "changeObjectName":
                 console.log(message);
                     this.waitingObjects.forEach((item, i) => {
-                        if (item.name !== message.data.name.name) return;
+                        if (item.name !== message.data.name) return;
                         item.state = 1;
                         item.respons = message.data;
                     });
